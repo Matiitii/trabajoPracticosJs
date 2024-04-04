f4abcb8ae04c7c62d190483e08f15145a2314ef6
Matiitii <166016459+Matiitii@users.noreply.github.com>
2024-04-04 18:32:45 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,59 @@
+
+
+
+
+/*Ejercicio 1, 2 y 3
+
+let Suma = 3 + 3 
+console.log ("La suma es "+ Suma)
+alert ("La suma es "+ Suma)
+
+let Resta = 4 - 2
+console.log ("La resta es "+ Resta)
+alert ("La resta es "+ Resta)
+
+ let Multiplicación = 4 * 2
+console.log ("La multiplicación es "+ Multiplicación)
+alert ("La multiplicación es "+ Multiplicación)
+
+let División = 4 / 2
+console.log ("La división es "+ División)
+alert ("La división es "+ División)
+
+*/
+
+
+
+/*Ejercicio 4 y 5
+
+let nacimiento = prompt ("Ingrese su año de nacimiento")
+let año = 2024
+let edadalumno = año - nacimiento
+let texto = "tu edad es "
+alert (texto + edadalumno)
+*/
+
+/*Ejercicio 6
+
+let NombreAlumno = prompt ("¡Ingresa tu nombre!")
+alert ("¡Bienvenido/a "+ NombreAlumno+ "!")
+
+*/
+
+/*Ejercicio 7
+
+let apellidoAlumno = prompt ("¡Ingresa tu apellido!")
+let nombreAlumno = prompt ("¡Ingresa tu nombre!")
+console.log ("Bienvenido/a "+ nombreAlumno+' '+  apellidoAlumno)
+alert ("Bienvenido/a "+ nombreAlumno+' '+ apellidoAlumno)
+*/
+
+
+//Ejercicio 8
+
+let Diez = 10
+let  Tres = 3
+let resultadoMultiplicación = Diez * Tres
+console.log (resultadoMultiplicación)
+alert (resultadoMultiplicación)
+
